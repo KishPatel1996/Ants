68c2b22b0ed0e4ee023c952c1fe0bc520e19337d
Kishan Patel <kishanpatel@Kishans-MacBook-Pro-2.local>
2014-10-13 14:52:48 -0700

    i fucked up

--- ants.py
+++ ants.py
@@ -68,6 +68,7 @@ class Insect:
     """An Insect, the base class of Ant and Bee, has armor and a Place."""
 
     is_ant = False
+    watersafe=False
 
     def __init__(self, armor, place=None):
         """Create an Insect with an armor amount and a starting Place."""
@@ -103,6 +104,7 @@ class Bee(Insect):
     """A Bee moves from place to place, following exits and stinging ants."""
 
     name = 'Bee'
+    watersafe=True
 
     def sting(self, ant):
         """Attack an Ant, reducing the Ant's armor by 1."""
@@ -436,6 +438,9 @@ class Water(Place):
         """Add insect if it is watersafe, otherwise reduce its armor to 0."""
         print('added', insect, insect.watersafe)
         "*** YOUR CODE HERE ***"
+        Place.add_insect(insect)
+        if insect.watersafe==False:
+          insect.reduce_armor(insect.armor)
 
 
 class FireAnt(Ant):
